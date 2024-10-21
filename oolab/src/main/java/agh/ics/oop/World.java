b0d35f676b81a0e30d921f891d8277f37e4822a2
Maja Sankowska <148463793+NiemySpiewak@users.noreply.github.com>
2024-10-21 15:55:15 +0200

    Update World.java

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -7,28 +7,25 @@ public class World
     public static void main(String[] args)
     {
         System.out.println("System wystartowal");
-        // Zamiana tablicy args na tablicę MoveDirection[] za pomocą OptionsParser
         MoveDirection[] directions = OptionsParser.parse(args);
-
-        // Wywołanie metody run z przekazaniem tablicy MoveDirection[]
         run(directions);
-        System.out.println("System zakonczyl dzialanie");
+        System.out.println("System skończył");
     }
     public static void run(MoveDirection[] directions)
     {
         for (MoveDirection direction : directions) {
             switch (direction) {
                 case FORWARD:
-                    System.out.println("Zwierzak idzie do przodu");
+                    System.out.println("Zwierzak do przodu");
                     break;
                 case BACKWARD:
-                    System.out.println("Zwierzak idzie do tylu");
+                    System.out.println("Zwierzak do tylu");
                     break;
                 case RIGHT:
-                    System.out.println("Zwierzak skreca w prawo");
+                    System.out.println("Zwierzak w prawo");
                     break;
                 case LEFT:
-                    System.out.println("Zwierzak skreca w lewo");
+                    System.out.println("Zwierzak w lewo");
                     break;
             }
         }
