da1d566add4a7b89445b0d5b2d9d9c83b467d104
Maja Sankowska <sankowskamaja25@gmail.com>
2024-10-26 20:59:26 +0200

    done

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,18 +1,41 @@
 package agh.ics.oop;
-
+import agh.ics.oop.model.Vector2d;
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.MapDirection;
 
 public class World
 {
     public static void main(String[] args)
     {
+        // Test metody toString
+        MapDirection direction = MapDirection.EAST;
+        System.out.println(direction);
+
+        // Test metody next
+        System.out.println(direction.next());
+        // Test metody previous
+        System.out.println(direction.previous());
+
+        // Test metody toUnitVector
+        Vector2d unitVector = direction.toUnitVector();
+        System.out.println(unitVector);
+
+
+
+
         System.out.println("System wystartowal");
-        // Zamiana tablicy args na tablicę MoveDirection[] za pomocą OptionsParser
         MoveDirection[] directions = OptionsParser.parse(args);
 
-        // Wywołanie metody run z przekazaniem tablicy MoveDirection[]
         run(directions);
         System.out.println("System zakonczyl dzialanie");
+
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+
     }
     public static void run(MoveDirection[] directions)
     {
