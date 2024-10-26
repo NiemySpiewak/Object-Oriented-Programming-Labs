6d0f72d74e16d2156e3da374d7508e76408b2245
Maja Sankowska <sankowskamaja25@gmail.com>
2024-10-26 21:08:02 +0200

    lab2 done

--- oolab/src/main/java/agh/ics/oop/OptionsParser.java
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -4,11 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 public class OptionsParser
 {
-    // Statyczna metoda zamieniająca tablicę String[] na MoveDirection[]
     public static MoveDirection[] parse(String[] args) {
         List<MoveDirection> directions = new ArrayList<>();
-
-        // Przetwarzanie każdego argumentu
         for (String arg : args) {
             switch (arg) {
                 case "f":
@@ -23,14 +20,10 @@ public class OptionsParser
                 case "l":
                     directions.add(MoveDirection.LEFT);
                     break;
-                // Niepoprawne argumenty są ignorowane
                 default:
-                    // Ignorujemy niepoprawne argumenty
                     break;
             }
         }
-
-        // Zamiana listy na tablicę MoveDirection[]
         return directions.toArray(new MoveDirection[0]);
     }
 }
